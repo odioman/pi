848ec6d543ef8372fc2f669e2adc6fef5685c764
odionman <ttkoshy@gmail.com>
2019-05-02 21:14:24 -0400

    first commit

---
+++ pi.rb
@@ -0,0 +1,180 @@
+begins = puts "Testing Pi"
+puts "Are you ready to begin?: 3 or n"
+userResp = gets
+
+  if userResp.match?(/n/)
+    puts begins
+  else userResp.match?(/3/)
+    puts "Enter Pi: "
+
+userGets1 = gets
+
+  if userGets1.match?(/1415926535/)
+    puts  "YES, Keep Going: "
+    puts itstime1 = Time.now
+  else puts "NO"
+    end
+
+userGets2 = gets
+
+  if userGets2.match?(/8979323846/)
+    puts  "YES, Keep Going: "
+    puts itstime2 = Time.now
+  else puts "NO"
+    end
+
+
+userGets3 = gets
+
+  if userGets3.match?(/2643383279/)
+    puts  "YES, Keep Going: "
+    puts itstime3 = Time.now
+  else puts "NO"
+    end
+
+
+userGets4 = gets
+
+  if userGets4.match?(/5028841971/)
+    puts  "YES, Keep Going: "
+    puts itstime4 = Time.now
+  else puts "NO"
+    end
+
+userGets5 = gets
+
+  if userGets5.match?(/6939937510/)
+    puts  "YES, Keep Going: "
+    puts itstime5 = Time.now
+  else puts "NO"
+    end
+
+userGets6 = gets
+
+  if userGets6.match?(/5820974944/)
+    puts  "YES, Keep Going: "
+    puts itstime6 = Time.now
+  else puts "NO"
+    end
+
+userGets7 = gets
+
+  if userGets7.match?(/5923078164/)
+    puts  "YES, Keep Going: "
+    puts itstime7 = Time.now
+  else puts "NO"
+    end
+
+userGets8 = gets
+
+  if userGets8.match?(/0628620899/)
+    puts  "YES, Keep Going: "
+    puts itstime8 = Time.now
+  else puts "NO"
+    end
+
+userGets9 = gets
+
+  if userGets9.match?(/8628034825/)
+    puts  "YES, Keep Going: "
+    puts itstime9 = Time.now
+  else puts "NO"
+    end
+
+userGets10 = gets
+
+  if userGets10.match?(/3421170679/)
+    puts  "YES, Keep Going: "
+    puts itstime10 = Time.now
+  else puts "NO"
+    end
+
+userGets11 = gets
+
+  if userGets11.match?(/8214808651/)
+    puts  "YES, Keep Going: "
+  puts itstime11 = Time.now
+  else puts "NO"
+    end
+
+userGets12 = gets
+
+  if userGets12.match?(/3282306647/)
+    puts  "YES, Keep Going: "
+    puts itstime12 = Time.now
+  else puts "NO"
+    end
+
+userGets13 = gets
+
+  if userGets13.match?(/0938446095/)
+    puts  "YES, Keep Going: "
+    puts itstime13 = Time.now
+  else puts "NO"
+    end
+
+userGets14 = gets
+
+  if userGets14.match?(/5058223172/)
+    puts  "YES, Keep Going: "
+    puts itstime14 = Time.now
+  else puts "NO"
+    end
+
+userGets15 = gets
+
+  if userGets15.match?(/5359408128/)
+    puts  "YES, Keep Going: "
+    puts itstime15 = Time.now
+  else puts "NO"
+    end
+
+userGets16 = gets
+
+  if userGets16.match?(/4811174502/)
+    puts  "YES, Keep Going: "
+    puts itstime16 = Time.now
+  else puts "NO"
+    end
+
+userGets17 = gets
+
+  if userGets17.match?(/8410270193/)
+    puts  "YES, Keep Going: "
+    puts itstime17 = Time.now
+  else puts "NO"
+    end
+
+userGets18 = gets
+
+  if userGets18.match?(/8521105559/)
+    puts  "YES, Keep Going: "
+    puts itstime18 = Time.now
+  else puts "NO"
+    end
+
+userGets19 = gets
+
+    if userGets19.match?(/6446229489/)
+      puts  "YES, Keep Going: "
+      puts itstime19 = Time.now
+    else puts "NO"
+      end
+userGets20 = gets
+
+      if userGets20.match?(/5493038196/)
+        puts  "YES, Keep Going: "
+        puts itstime20 = Time.now
+        puts (itstime20 - itstime1)/60
+      else puts "NO"
+      end
+
+userGets21 = gets
+
+      if userGets21.match?(/4428810975/)
+        puts "YES"
+        puts itstime21 = Time.now
+        puts (itstime21 - itstime1)/60
+      else puts "NO"
+      end
+end
